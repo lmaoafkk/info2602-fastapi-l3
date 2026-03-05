80992a42d05e61e91e09834c9d60d3080f140abb
lmaoafkk <ramsundarrondell@gmail.com>
2026-03-04 21:34:38 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -26,14 +26,14 @@ class TodoCategory(SQLModel, table=True):
 
 
 class Todo(SQLModel, table=True):
-    ## Task 2.1 implementation here. Remove the line below that says "pass" once completed
-    pass
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    text: str = Field(max_length=255)
+    done: bool = Field(default=False)
+    
 
-    ## Task 3.2 implementation should go here as well. Modify the class like you did for 3.1 above
-
-    ## Task 3.4 implementation should go here as well
-
-    # Task 5.2 code should go here
+    def toggle(self):
+        self.done = not self.done
     
     
 class Category(SQLModel, table=True):
