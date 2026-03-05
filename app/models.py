121795086995ff517990fb63d74ae1d37dbafcca
lmaoafkk <ramsundarrondell@gmail.com>
2026-03-04 21:49:22 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,41 +1,47 @@
 from sqlmodel import Field, SQLModel, Relationship
-from typing import Optional
+from typing import Optional, List
 from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
 
 class User(SQLModel, table=True):
-    id: Optional[int] =  Field(default=None, primary_key=True)
-    username:str = Field(index=True, unique=True)
-    email:str = Field(index=True, unique=True)
-    password:str
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    password: str
 
-    ## Task 3.1 code should go here (special care should go into the indentation)
-
-    ## End of task 3.1 code
+    todos: List['Todo'] = Relationship(back_populates="user")
 
     def set_password(self, plaintext_password):
         self.password = password_hash.hash(plaintext_password)
 
     def __str__(self) -> str:
-        return f"(User id={self.id}, username={self.username} ,email={self.email})"
+        return f"(User id={self.id}, username={self.username}, email={self.email})"
 
 class TodoCategory(SQLModel, table=True):
-    # Implementation of the TodoCategory model from task 5.1 here
-    pass
+    todo_id: Optional[int] = Field(default=None, foreign_key='todo.id', primary_key=True)
+    category_id: Optional[int] = Field(default=None, foreign_key='category.id', primary_key=True)
 
+class Category(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id')
+    text: str = Field(max_length=255)
+
+    
+    user: User = Relationship()
+    todos: List['Todo'] = Relationship(back_populates="categories", link_model=TodoCategory)
 
 class Todo(SQLModel, table=True):
-    id: Optional[int] =  Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id')
     text: str = Field(max_length=255)
     done: bool = Field(default=False)
+
     
+    user: User = Relationship(back_populates="todos")
+    categories: List['Category'] = Relationship(back_populates="todos", link_model=TodoCategory)
 
     def toggle(self):
         self.done = not self.done
     
     
-class Category(SQLModel, table=True):
-    # Implementation of the Category model from task 5.1 here
-    pass
